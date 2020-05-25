ca66e469a2e9ba5c9813a30ce08971cbd4b7ad26
Habibaudu <auduhabib1990@gmail.com>
2020-05-25 03:34:52 +0300

    -implement Login Feature - write few tests

--- users_app/url.py
+++ users_app/url.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from users_app.views import RegisterViewset
+from users_app.views import (RegisterViewset,LoginViewSet)
 
 
 router = DefaultRouter(trailing_slash = False)
 router.register(r'register',RegisterViewset, basename='user')
+router.register(r'login',LoginViewSet, basename='user')
 
 urlpatterns = [
     url(r'',include(router.urls))
